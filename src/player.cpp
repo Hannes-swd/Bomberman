31c8e90a944be2979bde70b21ea036546b46f728
leowe <leoweitlaner@gmail.com>
2026-04-08 16:36:47 +0200

    movement

--- src/player.cpp
+++ src/player.cpp
@@ -1,7 +1,29 @@
-#include "player.h"
+#include "player.h" // include playerclass from player.h
 
-Player::Player(int x, int y) : HP(10), speed(0), positionX(x), positionY(y) {}
+Player::Player(int x, int y) : HP(10), speed(0), positionX(x), positionY(y) {} // define player
 
+// functions
+// set bomb
 void Player::setBomb() {
     Bomb bomb(positionX, positionY);
+}
+
+// move up
+void Player::moveUp() {
+    positionY += 1;
+}
+
+// move Down
+void Player::moveDown() {
+    positionY -= 1;
+}
+
+// move left
+void Player::moveLeft() {
+    positionX -= 1;
+}
+
+// move right
+void Player::moveRight() {
+    positionX += 1;
 }
