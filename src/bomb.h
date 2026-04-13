02e11f8d9612869bcf5fcbe25dfce68c100a50e0
Hannes <hannes-swd@users.noreply.github.com>
2026-04-13 19:34:29 +0200

    bomb

--- src/bomb.h
+++ src/bomb.h
@@ -4,15 +4,26 @@
 #include "map.h"
 
 // define functions & struct
-void placeBomb(int x, int y);
+void placeBomb(int x, int y, int explosionRange);
 void drawBomb();
 void deleteBomb();
+void bombExplode(int posX, int posY, int explosionRange);
 
 struct Bomb {
     int posX;
     int posY;
     double spawnTime;
+    bool hasExploded;
+    int explosionRange;  // Individuelle Explosionsgröße für jede Bombe
 };
 
-// define list
+// Neue Variable für Explosionsdauer
+struct Explosion {
+    int posX;
+    int posY;
+    double startTime;
+};
+
+// define list - nur Deklarationen, keine Definitionen!
 extern std::vector<Bomb> BombList;
+extern std::vector<Explosion> ExplosionList;
